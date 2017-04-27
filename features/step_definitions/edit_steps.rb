9e9f590510822b32eea62e038d33ddd9ce9189d1
Robert Sedgwick <rasedgwick@gmail.com>
2017-04-27 19:25:08 +0000

    Updated Styling

--- features/step_definitions/edit_steps.rb
+++ features/step_definitions/edit_steps.rb
@@ -1,8 +1,7 @@
 Given(/^I am on edit page$/) do
-    visit 'locations#edit'
+    visit '/edit'
 end
 
 Then(/^I should see a list of update fields$/) do
-    expect(page).to have_field('Longitude')
     expect(page).to have_button('Submit')
 end
